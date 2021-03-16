f1cb9b4c4cc50e91743ea293f8296665dae9edba
Awake <52383201+Aw4k3@users.noreply.github.com>
2021-03-16 21:56:19 +0000

    Added/Updated Commands

--- FakeAwake/app.js
+++ FakeAwake/app.js
@@ -1,13 +1,16 @@
+const { randomInt } = require('crypto');
 const Discord = require('discord.js');
 const FileStream = require('fs');
 const WebClient = require('request');
-const sharp = require('sharp');
 const Sharp = require('sharp');
+//const GoogleAPI = require('googleapis');
 
 const Token = FileStream.readFileSync('D:/FakeAwake Token.txt', 'utf8');
+//const GoogleApiToken = FileStream.readFile('D:/FakeAwake GoogleAPI Token.json')
+
 const client = new Discord.Client();
 const prefix = '.';
-sharp.cache(false);
+Sharp.cache(false);
 
 //Setup bot functions
 var InvalidCommandMessage = 'User entered invalid command.';
@@ -34,10 +37,6 @@ function DownloadFile(uri, filename, callback) {
     });
 };
 
-client.on('ready', () => {
-    console.log(`Logged in as ${client.user.tag}!`);
-});
-
 var StatusColors = [
     '#ff0000', //Error - Red
     'ffff00',  //Warning - Yellow
@@ -57,6 +56,10 @@ var HelpTitles = [
 var MsgChainStack = [];
 var IsFirstMessage = true;
 var LastMessage;
+
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+});
 
 client.on('message', msg => {
 
@@ -91,28 +94,47 @@ client.on('message', msg => {
     switch (args[0]) {
         //Help command
         case 'help':
-            msg.channel.send(new Discord.MessageEmbed()
-                .setTitle(HelpTitles[RandInt(0, 4)])
-                .setColor('#7d46e3')
-                .addFields(
-                    //Utilites
-                    { name: 'about', value: 'Bot infos.' },
-                    { name: 'randnum', value: 'Generate a random int between a specified range.' },
+            switch (args[1]) {
+                default:
+                    msg.channel.send(new Discord.MessageEmbed()
+                        .setTitle(HelpTitles[RandInt(0, 4)])
+                        .setColor('#7d46e3')
+                        .addFields(
+                            //Utilites
+                            { name: 'about', value: 'Bot infos.' },
+                            { name: 'randnum', value: 'Generate a random int between a specified range.' },
+                            { name: 'sm', value: 'Provides string manipulation utilities.' },
 
-                    //Fun
-                    { name: 'awake', value: 'Get links to Awake\'s stuff (no only fans tho :sadge:).' },
-                    { name: 'ping', value: 'You get ponged.' },
-                    { name: 'bark', value: 'Woof!' },
-                    { name: 'roll', value: 'Returns a random number between 0 ~ 100' },
-                    { name: 'howgay', value: 'Check how much percent gay someone is.' },
-                    { name: 'howhorny', value: 'Check how much percent horny someone is.' },
-                    { name: 'ratemypp', value: 'Rate someone\' PP.' },
-                    { name: 'trap', value: 'Felix says Hi!' },
-                    { name: 'uwufy', value: 'UwUfy and sentence. You Weeb!' },
-                    { name: 'weewoo', value: 'Send mentioned users to horny jail.' }
-                )
-                .setFooter('Commands List')
-            );
+                            //Fun
+                            { name: 'awake', value: 'Get links to Awake\'s stuff (no only fans tho :sadge:).' },
+                            { name: 'ping', value: 'You get ponged.' },
+                            { name: 'bark', value: 'Woof!' },
+                            { name: 'roll', value: 'Returns a random number between 0 ~ 100' },
+                            { name: 'howgay', value: 'Check how much percent gay someone is.' },
+                            { name: 'howhorny', value: 'Check how much percent horny someone is.' },
+                            { name: 'ratemypp', value: 'Rate someone\' PP.' },
+                            { name: 'trap', value: 'Felix says Hi!' },
+                            { name: 'uwufy', value: 'UwUfy and sentence. You Weeb!' },
+                            { name: 'weewoo', value: 'Send mentioned users to horny jail.' }
+                        )
+                        .setFooter('Commands List')
+                    );
+                    break;
+                case 'sm':
+                    msg.channel.send(new Discord.MessageEmbed()
+                        .setTitle('String Manipulation')
+                        .setColor(StatusColors[0])
+                        .attachFiles(['./Assets/Images/StringManipulationIcon.png'])
+                        .setThumbnail('attachment://StringManipulationIcon.png')
+                        .addFields(
+                            { name: 'Bracket Definitions', value: '{Required} [optional]' },
+                            { name: prefix + 'sm [flag] {contents}', value: 'We all know you just don\'t want to look like the only person who can\'t type.' },
+                            { name: 'Available Flags', value: '-randomcase | -swapletters | -removeletters | -stroke' }
+                        )
+                        .setFooter(InvalidCommandMessage)
+                    );
+                    break;
+            }
             break;
 
         //Beat Saber
@@ -257,6 +279,66 @@ client.on('message', msg => {
             }
             break;
 
+        case 'sm': //String Manipluation
+            if (args.length > 1) {
+                var temp = msg.content.replace('.sm ', '');
+
+                var randomcase = msg.content.includes('-randomcase');
+                var swapletters = msg.content.includes('-swapletters');
+                var removeletters = msg.content.includes('-removeletters');
+                var stroke = msg.content.includes('-stroke'); //Insert smug face
+
+                if (randomcase) { temp = temp.replace('-randomcase', ''); }
+                if (swapletters) { temp = temp.replace('-swapletters', ''); }
+                if (removeletters) { temp = temp.replace('-removeletters', ''); }
+                if (stroke) { temp = temp.replace('-stroke', ''); randomcase = true; swapletters = true; removeletters = true; }
+
+                var StringResult = temp.trim().split('');
+
+                if (randomcase) { //Random Case Letters
+                    for (var i = 0; i < StringResult.length; i++) {
+                        if (randomInt(0, 2) > 0) { // 0.5 Chance
+                            StringResult[i] = StringResult[i].toUpperCase();
+                        }
+                    }
+                }
+
+                if (swapletters) {
+                    for (var i = 0; i < StringResult.length - 1; i++) {
+                        if (randomInt(0, 101) < 32) { // 0.32 Chance
+                            var Store = StringResult[i];
+                            StringResult[i] = StringResult[i + 1];
+                            StringResult[i + 1] = Store;
+                            i += 5; //Increment I at the same time to maintain readability of the string
+                        }
+                    }
+                }
+
+                if (removeletters) {
+                    for (var i = 0; i < temp.length - 1; i++) {
+                        if (randomInt(0, 101) < 20) { // 0.20 Chance
+                            StringResult.splice(i, 1);
+                        }
+                    }
+                }
+
+                msg.channel.send(StringResult.join(''));
+            } else {
+                msg.channel.send(new Discord.MessageEmbed()
+                    .setTitle('String Manipulation')
+                    .setColor(StatusColors[0])
+                    .attachFiles(['./Assets/Images/StringManipulationIcon.png'])
+                    .setThumbnail('attachment://StringManipulationIcon.png')
+                    .addFields(
+                        { name: 'Bracket Definitions', value: '{Required} [optional]' },
+                        { name: prefix + 'sm [flag] {contents}', value: 'We all know you just don\'t want to look like the only person who can\'t type.' },
+                        { name: 'Available Flags', value: '-randomcase | -swapletters | -removeletters | -stroke' }
+                    )
+                    .setFooter(InvalidCommandMessage)
+                );
+            }
+            break
+
         //Fun Commands
         case "awake":
             msg.channel.send(new Discord.MessageEmbed()
@@ -329,17 +411,72 @@ client.on('message', msg => {
             break;
 
         case 'howhorny':
+            var HornyRating = 0;
             if (msg.mentions.members.size) {
-                msg.mentions.members.forEach(user =>
+                msg.mentions.members.forEach(user => {
+                    //Log User
+                    var JObject = JSON.parse(FileStream.readFileSync('./Assets/Data/HornyJail.json', 'utf8'));
+                    console.log(JObject);
+                    if (!JObject['Inmates'].hasOwnProperty(user.user.username)) {
+                        JObject['Inmates'][user.user.username] = 1;
+                        console.log('[JSON] Logged ' + user.user.username + 'to ./Assets/Data/HornyJail.json with value' + JObject['Inmates'][user.user.username]);
+                    } else {
+                        JObject['Inmates'][user.user.username] = JObject['Inmates'][user.user.username] + 1;
+                    }
+
+                    //Rating
+                    HornyRating = RandInt(0, 101);
                     msg.channel.send(new Discord.MessageEmbed()
                         .setTitle('Horny Check')
                         .setColor('#ff00c3')
                         .attachFiles(['./Assets/Images/awakeGasm.png'])
                         .setThumbnail('attachment://awakeGasm.png')
                         .addFields(
-                            { name: user.displayName, value: RandInt(0, 101) + '% horny' }
+                            { name: user.displayName, value: HornyRating + '% horny' }
                         )
-                    ));
+                    );
+                    if (args[2] === '-trial' && parseInt(args[3]) < HornyRating) {
+                        //Download users profile image
+                        DownloadFile(user.user.avatarURL(), './Assets/temp/avatar128.webp', function () {
+                            console.log('[Download] Downloading profile image of ' + user.user.username);
+                            console.log('[Download] Avatar URL: ' + user.user.avatarURL());
+                            console.log('[Download] Downloaded profile image of ' + user.user.username);
+
+                            //Composite Images
+                            Sharp('./Assets/temp/avatar128.webp').resize(256, 256).toFile('./Assets/temp/avatar256.png').then(composite => {
+                                Sharp('./Assets/Images/PinkJailCell.png')
+                                    .composite([
+                                        {
+                                            input: './Assets/temp/avatar256.png',
+                                            gravity: 'center'
+                                        },
+                                        {
+                                            input: './Assets/Images/PinkCellBars.png'
+                                        }
+                                    ])
+                                    .toFile('./Assets/temp/comp.png', function (e) { console.log(e) });
+                            }).then(sendit => {
+                                //Send Embed
+                                msg.channel.send(new Discord.MessageEmbed()
+                                    .setTitle('Horny Jail')
+                                    .setDescription(user.displayName + ' has been weewoo\'d to horny jail')
+                                    .setColor('#ff00c3')
+                                    .attachFiles(['./Assets/Images/PinkJailCell.png', './Assets/temp/comp.png'])
+                                    .setThumbnail('attachment://PinkJailCell.png')
+                                    .setImage('attachment://comp.png')
+                                    .setFooter('This mans was a bit too horny Kappa')
+                                );
+
+                                /* Clean up */
+                                /* This bit breaks the bot, guess i will leave it commented out :( */
+                                //FileStream.unlinkSync('./Assets/temp/avatar128.webp');
+                                //FileStream.unlinkSync('./Assets/temp/avatar256.png');
+                                //FileStream.unlinkSync('./Assets/temp/comp.png');
+
+                            });
+                        });
+                    }
+                });
             } else {
                 msg.channel.send(new Discord.MessageEmbed()
                     .setTitle('Usage')
@@ -405,7 +542,7 @@ client.on('message', msg => {
         case 'uwufy':
             var uwufied = msg.content.replace('.uwufy', '');
             var replaceLetters = ['l', 'r']
-            
+
             //Do da ting
             for (var i = 0; i < replaceLetters.length; i++) {
                 for (var j = 0; j < uwufied.length; j++) {
@@ -419,12 +556,24 @@ client.on('message', msg => {
         case 'weewoo':
             if (msg.mentions.members.size) {
                 msg.mentions.members.forEach(user => {
+                    //Log User
+                    var JObject = JSON.parse(FileStream.readFileSync('./Assets/Data/HornyJail.json', 'utf8'));
+                    console.log(JObject);
+                    if (!JObject['Inmates'].hasOwnProperty(user.user.username)) {
+                        JObject['Inmates'][user.user.username] = 1;
+                        console.log('[JSON] Logged ' + user.user.username + 'to ./Assets/Data/HornyJail.json with value' + JObject['Inmates'][user.user.username]);
+                    } else {
+                        JObject['Inmates'][user.user.username] = JObject['Inmates'][user.user.username] + 1;
+                    }
+
+                    FileStream.writeFileSync('./Assets/Data/HornyJail.json', JSON.stringify(JObject));
+
                     //Download users profile image
                     DownloadFile(user.user.avatarURL(), './Assets/temp/avatar128.webp', function () {
                         console.log('[Download] Downloading profile image of ' + user.user.username);
                         console.log('[Download] Avatar URL: ' + user.user.avatarURL());
                         console.log('[Download] Downloaded profile image of ' + user.user.username);
-                        
+
                         //Composite Images
                         Sharp('./Assets/temp/avatar128.webp').resize(256, 256).toFile('./Assets/temp/avatar256.png').then(composite => {
                             Sharp('./Assets/Images/PinkJailCell.png')
@@ -450,12 +599,12 @@ client.on('message', msg => {
                                 .setFooter('This mans was a bit too horny Kappa')
                             );
 
-                        /* Clean up */
-                        /* This bit breaks the bot, guess i will leave it commented out :( */
+                            /* Clean up */
+                            /* This bit breaks the bot, guess i will leave it commented out :( */
                             //FileStream.unlinkSync('./Assets/temp/avatar128.webp');
                             //FileStream.unlinkSync('./Assets/temp/avatar256.png');
                             //FileStream.unlinkSync('./Assets/temp/comp.png');
-                            
+
                         });
                     });
                 });
@@ -471,8 +620,47 @@ client.on('message', msg => {
                 );
             }
             break;
+
+        case 'hornylog':
+            //List Users
+            var JObject = JSON.parse(FileStream.readFileSync('./Assets/Data/HornyJail.json', 'utf8'));
+            var HornyJailList = new Discord.MessageEmbed()
+                .setTitle('Horny Jail')
+                .setDescription('Look at all those hornies. Kappa')
+                .setColor('#ff00c3')
+                .attachFiles(['./Assets/Images/PinkJailCell.png'])
+                .setThumbnail('attachment://PinkJailCell.png')
+                .setFooter('Some of them even made it in more than once');
+
+            for (var object in JObject['Inmates']) {
+                HornyJailList.addField(object, 'Sentenced ' + JObject['Inmates'][object] + ' time(s)', true);
+            }
+            msg.channel.send(HornyJailList);
+            break;
+
+        case 'mute':
+            if (msg.member.roles.cache.has('821048545062813696')) {
+                if (msg.mentions.members.size > 0) {
+                    msg.mentions.members.forEach(user => {
+                        user.roles.add('821046922223419453');
+                        msg.channel.send(user.displayName + ' has been muted');
+                        var JObject = JSON.parse(FileStream.readFileSync('muted.json', 'utf8'));
+                        JObject[user.user.username] = user.id;
+                        FileStream.writeFileSync('muted.json', JSON.stringify(JObject));
+                    });
+                } else {
+                    msg.channel.send('No users were mentioned');
+                }
+            } else {
+                msg.reply('You ain\'t no admin, you sus!');
+            }
+            break;
     }
 
+});
+
+client.on('guildMemberAdd', member => {
+    member.guild.channels.cache.get('816384463579643904').send(`Welcome <@${guildMember.user.id}> to our server! Make sure to check out the rules channel!`);
 });
 
 client.login(Token);
